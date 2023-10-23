747e63dce1347c7ec1dab0eb1c7b1ae96dd715e6
Daniel Quintero <daqcarrillo@gmail.com>
2023-10-23 19:19:52 -0300

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./Redux/Store";
 import axios from "axios"
 // axios.defaults.baseURL = `http://localhost:3001`//esto dice que toda axios al momento de efectuarse tenga por defaults la base url http://localhost:3001 //este es el servidor de nuestro LOCAL
-axios.defaults.baseURL = `single-page-countries-backend-production.up.railway.app`
+axios.defaults.baseURL = `https://single-page-countries-backend-production.up.railway.app/`
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
